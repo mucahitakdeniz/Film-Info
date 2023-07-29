675f5528e526f73e9b1be8b6a1663c1f3343dbe3
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-29 10:27:01 +0300

    activeyear

--- Film-Info/src/components/FilmYear.jsx
+++ Film-Info/src/components/FilmYear.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 const FilmYear = ({ data, setDate }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const hanhleClick = (index) => {
+    setDate(data[index].date);
+    setActiveIndex(index);
+  };
   return (
     <div className="years">
-      {data.map((date) => (
-        <a onClick={() => setDate(date.date)} className="year">
+      {data.map((date, index) => (
+        <a
+          key={index}
+          onClick={() => hanhleClick(index)}
+          className="year"
+          style={{
+            color: activeIndex === index ? "red" : "black",
+            textDecorationLine: activeIndex === index ? "underline" : "none",
+          }}
+        >
           {date.date}
         </a>
       ))}
